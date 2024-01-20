89ddb84c4c80f68c1e5936b9ed177931f1b094a6
carahkatz <carah.katz@gmail.com>
2024-01-19 20:45:50 -0500

    Completed Lab 0 for Carah Katz Inserted ASCII art of initials in print lines

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,11 @@
 class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
-        
+        System.out.println(" _________    _________     ___   ___");
+        System.out.println("/     ____|  |   ___   \\   |   |/   /");
+        System.out.println("|    |       |   ___  _/   |       /");
+        System.out.println("|    |____   |  |   \\  \\   |   |\\  \\");
+        System.out.println("\\_________|  |__|    \\__\\  |___| \\__\\");
+
     }
 }
